230cdb2647fb8d7afd1f0b2033d6ceaf6fd5fedd
Balazs <lbalzs@aol.com>
2017-05-06 13:59:26 +0500

    Update Train.java

--- SzoftverProjektLaboratorium/src/projlab/Train.java
+++ SzoftverProjektLaboratorium/src/projlab/Train.java
@@ -8,26 +8,26 @@ import java.util.*;
 public class Train {
 
 	/**
-	 * A vagonokat tároló lista
+	 * A vagonokat tĂˇrolĂł lista
 	 */
 	private final List<TrainElement> elements = new ArrayList<TrainElement>();
 
 	/**
-	 * A mozdony jelenlegi tartózkodási helye
+	 * A mozdony jelenlegi tartĂłzkodĂˇsi helye
 	 */
 	private Tile pos;
 
 	/**
-	 * A mozdony előző tartózkodási helye
+	 * A mozdony elĹ‘zĹ‘ tartĂłzkodĂˇsi helye
 	 */
 	private Tile prevPos = null;
 
 	/**
 	 * @param pos
-	 *            a vonat pozíciója
+	 *            a vonat pozĂ­ciĂłja
 	 * @param color
-	 *            a vagonok színeit tároló tömb Konstruktor, létrehozza a
-	 *            vonatot a kapott paraméterekkel
+	 *            a vagonok szĂ­neit tĂˇrolĂł tĂ¶mb Konstruktor, lĂ©trehozza a
+	 *            vonatot a kapott paramĂ©terekkel
 	 */
 	public Train(Tile pos, List<Color> color) {
 		this.pos = pos;
@@ -42,15 +42,15 @@ public class Train {
 
 	/**
 	 * @param e
-	 *            Egy vagon Hozzáad egy paraméterként kapott vagont a tárolóhoz
+	 *            Egy vagon HozzĂˇad egy paramĂ©terkĂ©nt kapott vagont a tĂˇrolĂłhoz
 	 */
 	public void addElement(TrainElement e) {
 		elements.add(e);
 	}
 
 	/**
-	 * @return prevPos Az előző pozíció Visszaadja azt a mezőt ahol az előző
-	 *         ciklusban állt a mozdony
+	 * @return prevPos Az elĹ‘zĹ‘ pozĂ­ciĂł Visszaadja azt a mezĹ‘t ahol az elĹ‘zĹ‘
+	 *         ciklusban Ăˇllt a mozdony
 	 */
 	public Tile getPrevPos() {
 		return prevPos;
@@ -58,8 +58,8 @@ public class Train {
 
 	/**
 	 * @param pos
-	 *            Amező ahova a mozdonyt állítjuk Beállítja, hogy jelenleg hol
-	 *            áll a mozdony
+	 *            AmezĹ‘ ahova a mozdonyt ĂˇllĂ­tjuk BeĂˇllĂ­tja, hogy jelenleg hol
+	 *            Ăˇll a mozdony
 	 */
 	public void setPos(Tile pos) {
 		this.prevPos = this.pos;
@@ -67,11 +67,18 @@ public class Train {
 		this.pos = pos;
 		this.pos.setTrain(this);
 	}
+	
+	public boolean isTheTrainEmpty(){
+		for(int i = 0; i < elements.size(); i++){
+			if(!elements.get(i).isEmpty()) return false;
+		}
+		return true;
+	}
 
 	/**
 	 * @param pos
-	 *            A kovetkező mező Mozgatja a vonatot a következő mezőre,
-	 *            kijelöli hogy melyik vagonról szállhatnak le az utasok.
+	 *            A kovetkezĹ‘ mezĹ‘ Mozgatja a vonatot a kĂ¶vetkezĹ‘ mezĹ‘re,
+	 *            kijelĂ¶li hogy melyik vagonrĂłl szĂˇllhatnak le az utasok.
 	 */
 	public void moveTrain(Tile pos) {
 		this.prevPos = this.pos;
@@ -80,6 +87,7 @@ public class Train {
 		this.pos = pos;
 		this.pos.setTrain(this);
 		int j = 0;
+		
 		while (elements.get(j).isEmpty()) {
 			j++;
 			if(j == elements.size()) {
@@ -87,7 +95,6 @@ public class Train {
 				break;
 			}
 		}
-		
 		elements.get(j).SetNextToFree();
 		elements.get(0).move(this.prevPos);
 		for (int i = 1; i < elements.size(); i++) {
